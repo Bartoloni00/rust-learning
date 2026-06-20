517ab344e3a5154d55f472fb8c12baa90fec15a9
bartoloni00 <bartoloniabraham@gmail.com>
2026-06-20 08:37:59 -0300

    feat/list_proyects

--- devCenterCli/src/dev_commands.rs
+++ devCenterCli/src/dev_commands.rs
@@ -29,6 +29,29 @@ impl Proyect {
     pub fn to_json(&self) -> String {
         format!(r#"{{"id": {}, "name": "{}"}}"#, self.id, self.name)
     }
+
+    pub fn json_to_vec(json: &str) -> Vec<Proyect> {
+        let mut projects = Vec::new();
+        let json = json.trim_matches(|c| c == '[' || c == ']');
+        for item in json.split("},") {
+            println!("Parsing item: {}", item);
+            let item = item.trim_matches(|c| c == '{' || c == '}');
+            let mut id = 0;
+            let mut name = String::new();
+            for pair in item.split(',') {
+                let mut kv = pair.split(':');
+                let key = kv.next().unwrap().trim_matches('"').trim();
+                let value = kv.next().unwrap().trim_matches('"').trim();
+                match key {
+                    "id" => id = value.parse().unwrap(),
+                    "name" => name = value.to_string(),
+                    _ => (),
+                }
+            }
+            projects.push(Proyect { id, name });
+        }
+        projects
+    }
 }
 
 impl DevCenter {
@@ -54,18 +77,18 @@ impl DevCenter {
     }
 
     // %self indica que solo sera de lectura, ademas da a entender que no es una funcion estatica y puede llamarse con "." en lugar de "::"
-    pub fn list_proyects(&self) {
-        if self.projects.is_empty() {
-            println!("No projects found.");
-        } else {
-            println!("Projects:");
-            for proyect in &self.projects {
-                println!("- {}", proyect.name);
-            }
-        }
+    pub fn list_proyects(&self) -> () {
+        let data = fs::read_to_string("projects.json")
+            .expect("Unable to read file");
+        
+        println!("{}", data);
+
+        let projects = Proyect::json_to_vec(&data);
+
+        println!("Cantidad: {}", projects.len());
     }
 
-    pub fn create_proyect(&mut self, name: &str) {
+    pub fn create_proyect(&mut self, name: &str) -> () {
         let id = self.next_id;
         self.next_id += 1;
 
@@ -81,11 +104,11 @@ impl DevCenter {
         println!("Project '{}' created.", name);
     }
 
-    pub fn delete_proyect(&mut self, name: &str) {
+    pub fn delete_proyect(&mut self, name: &str) -> () {
         println!("Deleting project: {}", name);
     }
 
-    pub fn execute_proyect(&self, name: &str) {
+    pub fn execute_proyect(&self, name: &str) -> () {
         println!("Executing project: {}", name);
     }
 }
